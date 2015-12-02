b732988eb049be8b01fc6af2a493ab1702d047e6
Max Howell <mxcl@me.com>
2015-12-02 12:07:51 -0800

    Remove `targets: []`
    This is the default, but more importantly, I think it is confusing to have the example look like there are no targets, when in fact this just means: compute all targets.

--- Package.swift
+++ Package.swift
@@ -12,7 +12,6 @@ import PackageDescription
 
 let package = Package(
     name: "Dealer",
-    targets: [],
     dependencies: [
         .Package(url: "https://github.com/apple/example-package-deckofplayingcards.git", majorVersion: 1),
     ]
