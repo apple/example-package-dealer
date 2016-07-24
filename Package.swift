ca261c5a07d3d9748815697faf0f0394bbab8747
Anders Bertelrud <anders@apple.com>
2016-07-24 12:28:18 -0700

    Update required package versions of DeckOfPlayingCards dependency.

--- Package.swift
+++ Package.swift
@@ -13,6 +13,6 @@ import PackageDescription
 let package = Package(
     name: "Dealer",
     dependencies: [
-        .Package(url: "https://github.com/apple/example-package-deckofplayingcards.git", majorVersion: 2),
+        .Package(url: "https://github.com/apple/example-package-deckofplayingcards.git", majorVersion: 3),
     ]
 )
