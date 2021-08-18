40d6b55e5a4d2af895f1307bf7f07bbd28652d62
Mattt <mattt@me.com>
2021-08-18 13:05:12 -0700

    Update to swift-tools-version:5.3

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.3
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 /*
@@ -20,16 +20,22 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(url: "https://github.com/apple/example-package-deckofplayingcards.git", from: "3.0.0"),
+        .package(name: "DeckOfPlayingCards",
+                 url: "https://github.com/apple/example-package-deckofplayingcards.git",
+                 from: "3.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "Dealer",
-            dependencies: ["DeckOfPlayingCards"]),
+            dependencies: [
+                .byName(name: "DeckOfPlayingCards")
+            ]),
         .testTarget(
             name: "DealerTests",
-            dependencies: ["Dealer"]),
+            dependencies: [
+                .byName(name: "Dealer")
+            ]),
     ]
 )
