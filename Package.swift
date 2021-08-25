0c387452466b1b38e0d0cc431398a0ceb0967d8e
Mattt <mattt@me.com>
2021-08-25 08:57:45 -0700

    Remove name parameter in package dependency declarations

--- Package.swift
+++ Package.swift
@@ -23,11 +23,9 @@ let package = Package(
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
-        .package(name: "DeckOfPlayingCards",
-                 url: "https://github.com/apple/example-package-deckofplayingcards.git",
+        .package(url: "https://github.com/apple/example-package-deckofplayingcards.git",
                  from: "3.0.0"),
-        .package(name: "swift-argument-parser",
-                 url: "https://github.com/apple/swift-argument-parser.git",
+        .package(url: "https://github.com/apple/swift-argument-parser.git",
                  from: "0.4.4"),
     ],
     targets: [
@@ -37,7 +35,7 @@ let package = Package(
             name: "dealer",
             dependencies: [
                 .product(name: "DeckOfPlayingCards",
-                         package: "DeckOfPlayingCards"),
+                         package: "example-package-deckofplayingcards"),
                 .product(name: "ArgumentParser",
                          package: "swift-argument-parser")
             ]),
